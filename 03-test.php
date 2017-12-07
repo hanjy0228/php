fe5e1dac4df9acfce5c14afcff193fff9db558bc
hanjy0228 <sun20132220@163.com>
2017-12-07 15:34:14 +0800

    php

--- 03-test.php
+++ 03-test.php
@@ -5,8 +5,23 @@
  * Date: 2017/12/7
  * Time: 15:07
  */
-$name="aaabbb";
-var_dump($name);
-die();
-echo 123;
+if(isset($_POST['sub'])){
+//echo "success";
+    $ss=$_POST['num'];
+    $arr=arry(1,2);
+//    unset($ss);
+    print_r($arr);
+//    echo $ss;
+}
+else {
+    //echo "erro";
+
+}
 ?>
+
+<meta charset="utf-8">
+
+<form action="03-test.php" method="post">
+    数据：<input type="text" name="num">
+    <input type="submit" name="sub" value="提交">
+</form>
---
+++ 04-test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HanJingYi
+ * Date: 2017/12/7
+ * Time: 15:13
+ */
+?>
